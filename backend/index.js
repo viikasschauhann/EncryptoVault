27f31cdb4ddb70625ae9234e070876ce1594bc1d
viikasschauhann <vchauhan6155@gmail.com>
2025-11-28 20:01:55 +0530

    adding vercel.json file

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
